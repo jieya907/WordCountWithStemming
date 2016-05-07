8c04ad4ef2ef19e14f81d808af2cc8f039ba2f30
jieya907 <wenjieyas@gmail.com>
2016-05-07 14:25:08 -0500

    able to compare snowball stem and lancaster stem

--- stemcompare.py
+++ stemcompare.py
@@ -1,11 +1,53 @@
+import operator 
 from collections import defaultdict
 import nltk
 import argparse
 from nltk import stem
 from nltk import word_tokenize
 import pprint
+import sys
 
-lc = stem.lancaster.LancasterStemmer()
+#This is to get rid of the utf-8 and ascii encoding problem
+reload(sys)
+sys.setdefaultencoding('utf8')
+print sys.getdefaultencoding()
+
+CUTOFF = 50
+
+def basecnt(words):
+    """
+    Give a list of words, count them to a dictionary. Word to word count
+    """
+    bsct = defaultdict(int)
+    for j in words:
+        bsct [j] += 1
+    sort_cnt = sorted(bsct.items(), key = operator.itemgetter(1))
+    sort_cnt.reverse()
+    return sort_cnt
+
+def lcstem(words):
+    """
+    Give a list of words, using the Lancaster stemmer to stem the words
+    then output a list of tuples, first item is the word, second is the 
+    occurrence of the word 
+    """
+    lc = stem.lancaster.LancasterStemmer()
+    # tokenizing using lancaster stemmer 
+    lctk = [lc.stem(i) for i in words]
+    return basecnt(lctk)
+
+def sbstem(words):
+    """
+    Give a list of words, using the snowball stemmer to stem the words
+    then output a list of tuples, first item is the word, second is the 
+    occurrence of the word 
+    """
+    lc = stem.snowball.SnowballStemmer("english")
+    # tokenizing using lancaster stemmer 
+    lctk = [lc.stem(i) for i in words]
+    return basecnt(lctk)
+
+
 
 parser = argparse.ArgumentParser(description = 'Process a text file.')
 
@@ -18,15 +60,21 @@ print args
 filename = args.filename
 pp = open(filename)
 
+# string of the text 
 text = pp.read()
 
+# the list of words 
 tokens = word_tokenize(text)
 
-lctk = [lc.stem(i) for i in tokens]
+allwd = basecnt(tokens)
+print "all words in the text, without stemming"
+print allwd[:CUTOFF] 
+#
+print "words in text with snow ball stemming" 
+sbwd = sbstem(tokens)
+print sbwd[:CUTOFF]
 
-lcct = defaultdict(int)
+print "words in text with lancasterstemming" 
+lcwd = lcstem(tokens)
+print lcwd[:CUTOFF]
 
-for j in lctk:
-    lcct[j] += 1
-
-pprint.pprint(dict(lcct))
